e30bf58d58827327d4c9835660a38fac6d1dd55f
mortzu <mortzu@gmx.de>
2015-07-05 19:18:24 +0200

    First step to introduce API

--- public/index.php
+++ public/index.php
@@ -5,12 +5,8 @@ require_once realpath(__DIR__ . '/../config.default.php');
 if (file_exists(realpath(__DIR__ . '/../config.php')))
   require_once realpath(__DIR__ . '/../config.php');
 
-// check if libvirt-php is installed
-if (!function_exists('libvirt_connect')) {
-  header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500);
-  error_log("libvirt-php is not installed!\n");
-  exit(1);
-}
+if (!isset($_SERVER['REMOTE_USER']) && isset($_SERVER['PHP_AUTH_USER']))
+  $_SERVER['REMOTE_USER'] = $_SERVER['PHP_AUTH_USER'];
 
 // if remote user not set send 500 HTTP status
 if (!isset($_SERVER['REMOTE_USER'])) {
@@ -39,6 +35,47 @@ if (is_dir(realpath(__DIR__ . '/../functions')))
     if (is_file($file))
       require_once $file;
 
+function ip_is_private($ip) {
+  $ip = explode('.', $ip);
+  $a = (int) $ip[0];
+  $b = (int) $ip[1];
+  $c = (int) $ip[2];
+  $d = (int) $ip[3];
+
+  if ($a === 10)
+    return true;
+
+  if ($a === 192 && $b === 168)
+    return true;
+
+  if ($a !== 172)
+    return false;
+
+  if ($b >= 16 && $b <= 31)
+    return true;
+
+  return false;
+}
+
+function api_call($url) {
+  // create a new cURL resource
+  $ch = curl_init();
+
+  // set URL
+  curl_setopt($ch, CURLOPT_URL, $url);
+
+  // return the content
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+  // grab URL and pass it to the browser
+  $data = curl_exec($ch);
+
+  // close cURL resource, and free up system resources
+  curl_close($ch);
+
+  return $data;
+}
+
 function formatBytes($bytes, $precision = 2) {
   $units = array('B', 'KB', 'MB', 'GB', 'TB');
 
@@ -62,57 +99,49 @@ $domains = array();
    and informations about virtual servers
 */
 foreach ($config['domain_hosts'] as $domain_host) {
-  // get domain list
-  $domain_names = wrap_libvirt_domain_list($domain_host);
+  // grab URL and pass it to the browser
+  $domain_host_data = @json_decode(api_call($domain_host . '/?action=get_domains'), true);
 
-  // get informations about the host
-  $domain_host_info = @wrap_libvirt_conn_informations($domain_host);
-
-  // get hostname of the host
-  $domain_host_hostname = wrap_libvirt_conn_hostname($domain_host);
+  // if decoding failed continue
+  if (!is_array($domain_host_data))
+    continue;
 
   // iterate over domains of host
-  foreach ($domain_names as $domain_name) {
+  foreach ($domain_host_data as $domain) {
+    // check if this is a domain
+    if (!isset($domain['state']))
+      continue;
+
     /* check if user is staff
        or has the right to administrate this domain
     */
-    if ((!isset($config['rights'][$domain_host_hostname][$domain_name]) ||
-         !is_array($config['rights'][$domain_host_hostname][$domain_name]) ||
-         !in_array($_SERVER['REMOTE_USER'], $config['rights'][$domain_host_hostname][$domain_name])) &&
+    if ((!isset($config['rights'][$domain_host_data['hostname']][$domain['name']]) ||
+         !is_array($config['rights'][$domain_host_data['hostname']][$domain['name']]) ||
+         !in_array($_SERVER['REMOTE_USER'], $config['rights'][$domain_host_data['hostname']][$domain['name']])) &&
          !in_array($_SERVER['REMOTE_USER'], $config['staff_member']))
       continue;
 
-    // get informations about domain
-    $domain_info = wrap_libvirt_domain_info($domain_host, $domain_name);
-
-    // get XML description
-    $domain_xml_string = wrap_libvirt_domain_xml($domain_host, $domain_name);
-
-    // and parse it
-    $domain_xml = new SimpleXMLElement($domain_xml_string);
-
     // set variables
-    $domains[$domain_name]['status'] = wrap_libvirt_domain_running($domain_host, $domain_name);
-    $domains[$domain_name]['host_uri'] = $domain_host;
-    $domains[$domain_name]['host_hostname'] = $domain_host_hostname;
-    $domains[$domain_name]['hypervisor'] = $domain_host_info['hypervisor'];
-    $domains[$domain_name]['vcpu_current'] = $domain_info['nrVirtCpu'];
-    $domains[$domain_name]['vcpu_max'] = $domain_host_info['hypervisor_maxvcpus'];
-    $domains[$domain_name]['memory_current'] = ($domain_host_info['hypervisor'] == 'OpenVZ') ? NULL : $domain_info['memory'];
-    $domains[$domain_name]['memory_max'] = ($domain_host_info['hypervisor'] == 'OpenVZ') ? NULL : $domain_info['maxMem'];
-    $domains[$domain_name]['vnc_port'] = isset($domain_xml->devices[0]->graphics['port']) ? $domain_xml->devices[0]->graphics['port'] : NULL;
-    $domains[$domain_name]['vnc_listen'] = isset($domain_xml->devices[0]->graphics['listen']) ? $domain_xml->devices[0]->graphics['listen'] : NULL;
+    $domains[$domain_host_data['hostname']][$domain['name']]['state'] = ($domain['state'] == 'running' ? true : false);
+    $domains[$domain_host_data['hostname']][$domain['name']]['id'] = $domain['id'];
+    $domains[$domain_host_data['hostname']][$domain['name']]['host_uri'] = $domain_host;
+    $domains[$domain_host_data['hostname']][$domain['name']]['host_hostname'] = $domain['name'];
+    $domains[$domain_host_data['hostname']][$domain['name']]['hypervisor'] = $domain_host_data['hypervisor'];
+    $domains[$domain_host_data['hostname']][$domain['name']]['vcpu'] = $domain['vcpu'];
+    $domains[$domain_host_data['hostname']][$domain['name']]['memory'] = $domain['memory'];
+    $domains[$domain_host_data['hostname']][$domain['name']]['vnc_port'] = isset($domain['vnc_port']) ? $domain['vnc_port'] : NULL;
+    $domains[$domain_host_data['hostname']][$domain['name']]['vnc_address'] = isset($domain['vnc_address']) ? $domain['vnc_address'] : NULL;
 
-    // unset variables
-    unset($domain_info);
-    unset($domain_xml_string);
-    unset($domain_xml);
+    if (isset($domain['ip']) && is_array($domain['ip']))
+      $domains[$domain_host_data['hostname']][$domain['name']]['ip_assignment'] = $domain['ip'];
+    elseif (isset($config['ip_assignment'][$domain['name']]) && is_array($config['ip_assignment'][$domain['name']]))
+      $domains[$domain_host_data['hostname']][$domain['name']]['ip_assignment'] = $config['ip_assignment'][$domain['name']];
+    else
+      $domains[$domain_host_data['hostname']][$domain['name']]['ip_assignment'] = NULL;
   }
 
   // unset variables
-  unset($domain_names);
-  unset($domain_host_info);
-  unset($domain_host_hostname);
+  unset($domain_host_data);
 }
 
 // sort array by keys
@@ -122,12 +151,15 @@ ksort($domains);
 if (isset($_GET['domain_name']) && isset($_GET['action'])) {
   $domain_name = $_GET['domain_name'];
 
-  // check if host of domain is set
-  if (!isset($domains[$domain_name]['host_hostname']))
-    $msg_error = 'Host of domain not set!';
-  else {
-    $domain_host_hostname = $domains[$domain_name]['host_hostname'];
+  foreach ($domains as $host_hostname => $domain)
+    foreach ($domain as $tmp_domain_name => $tmp_domain_info)
+      if ($tmp_domain_name == $domain_name) {
+        $domain_host_hostname = $host_hostname;
+        $domain_host_uri = $tmp_domain_info['host_uri'];
+      }
 
+  // check if host of domain is set
+  if (!empty($domain_host_hostname)) {
     /* check if user is staff
        or has the right to administrate this domain
     */
@@ -139,32 +171,22 @@ if (isset($_GET['domain_name']) && isset($_GET['action'])) {
     else {
       switch($_GET['action']) {
         case 'domain_start':
-          if (wrap_libvirt_domain_start($domains[$domain_name]['host_uri'], $domain_name))
-            $msg_success = 'Domain started successfully!';
-          else
-            $msg_error = 'Failed to start domain!';
-          break;
         case 'domain_shutdown':
-          if (wrap_libvirt_domain_shutdown($domains[$domain_name]['host_uri'], $domain_name))
-            $msg_success = 'Domain shutdowned successfully!';
-          else
-            $msg_error = 'Failed to shutdown domain!';
-          break;
         case 'domain_reboot':
-          if (wrap_libvirt_domain_reboot($domains[$domain_name]['host_uri'], $domain_name))
-            $msg_success = 'Domain rebooted successfully!';
-          else
-            $msg_error = 'Failed to reboot domain!';
-          break;
         case 'domain_reset':
-          if (wrap_libvirt_domain_reset($domains[$domain_name]['host_uri'], $domain_name))
-            $msg_success = 'Domain resetted successfully!';
+          if ($domains[$domain_host_hostname][$domain_name]['hypervisor'] == 'OpenVZ')
+            $data = api_call($domain_host_uri . '/?action=' . $_GET['action'] . '&name=' . $domains[$domain_host_hostname][$domain_name]['id']);
           else
-            $msg_error = 'Failed to reset domain!';
+            $data = api_call($domain_host_uri . '/?action=' . $_GET['action'] . '&name=' . $domain_name);
+
+          if ($data['type'] == 'success')
+            $msg_success = 'Successful!';
+          else
+            $msg_error = 'Failed!';
           break;
         case 'domain_console':
           // write token file for noVNC
-          file_put_contents(realpath(__DIR__ . '/../novnc_token') . '/' . substr(md5($domain_name), 0, 10), substr(md5($domain_name), 0, 10) . ': ' . $domain_host_hostname . ':' . $domains[$domain_name]['vnc_port']);
+          file_put_contents(realpath(__DIR__ . '/../novnc_token') . '/' . substr(md5($domain_name), 0, 10), substr(md5($domain_name), 0, 10) . ': ' . $domain_host_hostname . ':' . $domains[$domain_host_hostname][$domain_name]['vnc_port']);
 
           // return content of noVNC
           echo str_replace('{{{TITLE}}}', $config['title'], file_get_contents(realpath(__DIR__ . '/../templates/parts/vnc.tmpl')));
@@ -174,15 +196,16 @@ if (isset($_GET['domain_name']) && isset($_GET['action'])) {
           break;
       }
     }
-  }
+  } else
+    $msg_error = 'Host of domain not set!';
 }
 
 $site_content = '<h2>Domains</h2>';
 
 // display status messages
-if (isset($msg_success) && $msg_success != '')
+if (isset($msg_success) && !empty($msg_success))
   $site_content .= '<div class="alert alert-success" role="alert">' . $msg_success . "</div>\n";
-if (isset($msg_error) && $msg_error != '')
+if (isset($msg_error) && !empty($msg_error))
   $site_content .= '<div class="alert alert-danger" role="alert">' . $msg_error . "</div>\n";
 
 $site_content .= "<table class=\"table table-striped\">\n";
@@ -193,38 +216,52 @@ $site_content .= "  <th>Status</th>\n";
 $site_content .= "  <th>VCPUs</th>\n";
 $site_content .= "  <th>Memory</th>\n";
 $site_content .= "  <th>Hypervisor</th>\n";
+$site_content .= "  <th>IPs</th>\n";
 $site_content .= "  <th>Tasks</th>\n";
 $site_content .= "</tr>\n";
 
 // iterate over domains array
-foreach ($domains as $domain_name => $domain_info) {
-  $site_content .= "<tr>\n";
+foreach ($domains as $host_hostname => $host_domains) {
+  foreach ($host_domains as $domain_name => $domain_info) {
+    $site_content .= "<tr>\n";
 
-  if (isset($config['alias'][$domain_info['host_hostname']][$domain_name]) &&
+    if (isset($config['alias'][$domain_info['host_hostname']][$domain_name]) &&
       $config['alias'][$domain_info['host_hostname']][$domain_name] != '')
-    $site_content .= '<td>' . $config['alias'][$domain_info['host_hostname']][$domain_name] . "</td>\n";
-  else
-    $site_content .= '<td>' . $domain_name . "</td>\n";
+      $site_content .= '<td>' . $config['alias'][$domain_info['host_hostname']][$domain_name] . "</td>\n";
+    else
+      $site_content .= '<td>' . $domain_name . "</td>\n";
 
-  $site_content .= '<td><span id="status" class="label label-' . (($domain_info['status'] == true) ? 'success">running' : 'danger">stopped') . "</span></td>\n";
-  $site_content .= '<td>' . $domain_info['vcpu_current'] . ' / ' . $domain_info['vcpu_max'] . "</td>\n";
-  $site_content .= '<td>' . (($domain_info['memory_current'] == NULL) ? ' - ' : formatBytes($domain_info['memory_current'] * 1024) . (($domain_info['memory_current'] != $domain_info['memory_max']) ? ' / ' . formatBytes($domain_info['memory_max'] * 1024) : '')) . "</td>\n";
-  $site_content .= '<td>' . $domain_info['hypervisor'] . "</td>\n";
+    $site_content .= '<td><span id="status" class="label label-' . (($domain_info['state'] == true) ? 'success">running' : 'danger">stopped') . "</span></td>\n";
+    $site_content .= '<td>' . $domain_info['vcpu'] . "</td>\n";
+    $site_content .= '<td>' . (($domain_info['memory'] == NULL) ? ' - ' : formatBytes($domain_info['memory'] * 1024)) . "</td>\n";
+    $site_content .= '<td>' . $domain_info['hypervisor'] . "</td>\n";
 
-  $site_content .= "<td>\n";
-  $site_content .= "<a href=\"" . $_SERVER['REQUEST_URI'] . "?action=domain_start&domain_name=" . $domain_name . "\" class=\"btn btn-default" . (($domain_info['status'] == true) ? ' disabled' : '') . "\" data-container=\"body\" data-toggle=\"tooltip\" data-placement=\"bottom\" title=\"Boot\"> <span class=\"glyphicon glyphicon-play\"></span> </a>\n";
-  $site_content .= "<a href=\"" . $_SERVER['REQUEST_URI'] . "?action=domain_shutdown&domain_name=" . $domain_name . "\" class=\"btn btn-default" . (($domain_info['status'] != true) ? ' disabled' : '') . "\" data-container=\"body\" data-toggle=\"tooltip\" data-placement=\"bottom\" title=\"Shutdown\"> <span class=\"glyphicon glyphicon-off\"></span> </a>\n";
-  $site_content .= "<a href=\"" . $_SERVER['REQUEST_URI'] . "?action=domain_reboot&domain_name=" . $domain_name . "\" class=\"btn btn-default\" data-container=\"body\" data-toggle=\"tooltip\" data-placement=\"bottom\" title=\"Reboot\"> <span class=\"glyphicon glyphicon-refresh\"></span> </a>\n";
-  $site_content .= "<a href=\"" . $_SERVER['REQUEST_URI'] . "?action=domain_reset&domain_name=" . $domain_name . "\" class=\"btn btn-default\" data-container=\"body\" data-toggle=\"tooltip\" data-placement=\"bottom\" title=\"Hard Reset\"> <span class=\"glyphicon glyphicon-fire\"></span> </a>\n";
+    $site_content .= "<td>\n";
+    if (is_array($domain_info['ip_assignment']))
+      foreach ($domain_info['ip_assignment'] as $address)
+        if (!ip_is_private($address))
+          $site_content .= '<span data-toggle="tooltip" data-placement="top" title="rDNS: ' . gethostbyaddr($address) . '">' . $address . '</span><br />';
+        else
+          $site_content .= $address . '<br />';
+    else
+      $site_content .= ' - ';
+    $site_content .= "</td>\n";
 
-  /* display link to console only if domain has VNC port set
-     and listen address is not localhost
-   */
-  if ($domain_info['vnc_port'] != NULL && (isset($domain_info['vnc_listen']) && $domain_info['vnc_listen'] != '127.0.0.1'))
-    $site_content .= "<a onclick=\"window.open('" . $_SERVER['REQUEST_URI'] . "?action=domain_console&domain_name=" . $domain_name . "&token=" . substr(md5($domain_name), 0, 10) . "')\" class=\"btn btn-default\" data-container=\"body\" data-toggle=\"tooltip\" data-placement=\"bottom\" title=\"Console\"> <span class=\"glyphicon glyphicon-blackboard\"></span> </a>\n";
+    $site_content .= "<td>\n";
+    $site_content .= "<a href=\"" . $_SERVER['REQUEST_URI'] . "?action=domain_start&domain_name=" . $domain_name . "\" class=\"btn btn-default" . (($domain_info['state'] == true) ? ' disabled' : '') . "\" data-container=\"body\" data-toggle=\"tooltip\" data-placement=\"bottom\" title=\"Boot\"> <span class=\"glyphicon glyphicon-play\"></span> </a>\n";
+    $site_content .= "<a href=\"" . $_SERVER['REQUEST_URI'] . "?action=domain_shutdown&domain_name=" . $domain_name . "\" class=\"btn btn-default" . (($domain_info['state'] != true) ? ' disabled' : '') . "\" data-container=\"body\" data-toggle=\"tooltip\" data-placement=\"bottom\" title=\"Shutdown\"> <span class=\"glyphicon glyphicon-off\"></span> </a>\n";
+    $site_content .= "<a href=\"" . $_SERVER['REQUEST_URI'] . "?action=domain_reboot&domain_name=" . $domain_name . "\" class=\"btn btn-default\" data-container=\"body\" data-toggle=\"tooltip\" data-placement=\"bottom\" title=\"Reboot\"> <span class=\"glyphicon glyphicon-refresh\"></span> </a>\n";
+    $site_content .= "<a href=\"" . $_SERVER['REQUEST_URI'] . "?action=domain_reset&domain_name=" . $domain_name . "\" class=\"btn btn-default" . (($domain_info['hypervisor'] == 'OpenVZ') ? ' disabled' : '') . "\" data-container=\"body\" data-toggle=\"tooltip\" data-placement=\"bottom\" title=\"Hard Reset\"> <span class=\"glyphicon glyphicon-fire\"></span> </a>\n";
 
-  $site_content .= "</td>\n";
-  $site_content .= "</tr>\n";
+    /* display link to console only if domain has VNC port set
+       and listen address is not localhost
+     */
+    if ($domain_info['vnc_port'] != NULL && (isset($domain_info['vnc_address']) && $domain_info['vnc_address'] != '127.0.0.1'))
+      $site_content .= "<a onclick=\"window.open('" . $_SERVER['REQUEST_URI'] . "?action=domain_console&domain_name=" . $domain_name . "&token=" . substr(md5($domain_name), 0, 10) . "')\" class=\"btn btn-default\" data-container=\"body\" data-toggle=\"tooltip\" data-placement=\"bottom\" title=\"Console\"> <span class=\"glyphicon glyphicon-blackboard\"></span> </a>\n";
+
+    $site_content .= "</td>\n";
+    $site_content .= "</tr>\n";
+  }
 }
 
 $site_content .= "</table>\n";
